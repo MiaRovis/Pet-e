42e80ecfa17574de08961b8f398cf6bed7dc754f
Mia Rovis <mia.rovis@gmail.com>
2024-01-11 19:11:21 +0100

    ispravak mongo

--- app/db.py
+++ app/db.py
@@ -9,7 +9,7 @@ def get_database() -> DataBase:
     return db
 
 async def connect_to_mongo():
-    db.client = AsyncIOMotorClient("mongodb+srv://admin:<admin>@cluster0.boinvpz.mongodb.net/?retryWrites=true&w=majority")
+    db.client = AsyncIOMotorClient("mongodb+srv://admin:admin@cluster0.boinvpz.mongodb.net/?retryWrites=true&w=majority")
     db.mongodb = db.client["Pet-e"]
 
 async def close_mongo_connection():
